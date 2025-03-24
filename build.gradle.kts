40518680c597650c68f3a4ee5b076a626eb5721a
Michael Ostanin <ostaninmiha7@gmail.com>
2025-03-24 15:46:53 +0300

    Added retry mechanic to app

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework.boot:spring-boot-starter-aop:2.6.4")
+	implementation("org.springframework.retry:spring-retry:2.0.11")
 }
 
 dependencyManagement {
